7999d90e36cdf933152ced0ab662134d1ffeae0e
scardinius <scardinius@chords.pl>
2016-02-24 14:04:31 +0100

    Add setting Membership Group ID

---
+++ settings/communitysize.setting.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+  'member_group_id' => array(
+    'group_name' => 'Community Size',
+    'group' => 'communitysize',
+    'name' => 'member_group_id',
+    'type' => 'Integer',
+    'quick_form_type' => 'Element',
+    'html_type' => 'text',
+    'default' => 42,
+    'add' => '4.6',
+    'is_domain' => 1,
+    'is_contact' => 0,
+    'title' => 'Membership Group ID',
+    'description' => 'Membership of this group means that contact is in our community.',
+    'help_text' => 'Membership of this group means that contact is in our community.',
+  ),
+);
